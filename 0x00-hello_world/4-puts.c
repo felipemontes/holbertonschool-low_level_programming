28a3c97291b471b34e335facd209dfc2d5e8aa00
felipe <felipe_londono1@hotmail.com>
2019-09-18 19:46:04 +0000

    Added comment structure

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(void) /*Prints string*/
+/**
+* main - This program is about printing using puts
+* Return: probando
+*/
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
