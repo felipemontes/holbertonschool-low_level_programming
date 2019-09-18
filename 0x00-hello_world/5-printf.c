712bf77f564526f052008ff1efc8ecafe4f307d7
felipe <felipe_londono1@hotmail.com>
2019-09-18 19:48:08 +0000

    Added comment structure

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - This program is about printing using puts
+ * Return: probando
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
