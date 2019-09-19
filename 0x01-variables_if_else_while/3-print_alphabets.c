f0d81ba2c6f22b7ffc2f308444051fe6b61ac999
felipe <felipe_londono1@hotmail.com>
2019-09-19 17:41:42 +0000

    Program to show alphabet in upper and lowercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - This program is about random numbers
+* Return: It returns positive or negative
+*/
+int main(void)
+{
+char a = a;
+char A = a;
+for (a = 'a'; a <= 'z'; a++)
+putchar(a);
+for (A = 'A'; A <= 'Z'; A++)
+putchar(A);
+putchar('\n');
+return (0);
+}
+
