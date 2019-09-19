548b58b09680d390058582ade7fadce17b8c810f
felipe <felipe_londono1@hotmail.com>
2019-09-19 20:50:40 +0000

    Program that print alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - This program is about random numbers
+* Return: It returns positive or negative
+*/
+int main(void)
+{
+char a = 'z';
+while (a >= 'a')
+{
+putchar(a);
+a--;
+}
+putchar('\n');
+return (0);
+}
