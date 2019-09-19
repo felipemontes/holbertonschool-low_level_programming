41908a50f6b61b63e177be6dfafd39257d033837
felipe <felipe_londono1@hotmail.com>
2019-09-19 17:27:05 +0000

    Program to print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - This program is about random numbers
+ * Return: It returns positive or negative
+ */
+int main(void)
+{
+char a = a;
+for (a = 'a'; a <= 'z'; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
