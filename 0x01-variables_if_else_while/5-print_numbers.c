59122aff57f0671a98bf6bb0df749c92b4a60c33
felipe <felipe_londono1@hotmail.com>
2019-09-19 19:46:41 +0000

    Program to print base 10 numbers starting with 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - This program is about random numbers
+* Return: It returns positive or negative
+*/
+int main(void)
+{
+int a = a;
+for (a = 0; a <= 9; a++)
+printf("%i", a);
+printf("\n");
+return (0);
+}
