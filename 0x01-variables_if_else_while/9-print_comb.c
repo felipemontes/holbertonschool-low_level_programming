a6b42daba6eca43e0fa9fe113dda15abee529497
felipe <felipe_londono1@hotmail.com>
2019-09-19 21:27:25 +0000

    Program to print multiple combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+int a = '0';
+while (a <= '9')
+{
+putchar(a);
+if(a == '9')
+{
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+a++;
+}
+putchar('\n');
+
+return (0);
+}
