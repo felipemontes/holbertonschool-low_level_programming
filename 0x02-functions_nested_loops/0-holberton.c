62184ab14bc7a53e5a0b7e8fe916b1526ed8341b
felipe <felipe_londono1@hotmail.com>
2019-09-24 13:49:27 +0000

    Program that prints holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+* main - This program shows letters
+* Return: returns
+*/
+int main(void)
+{
+	int h = 'H';
+	int o = 'o';
+	int l = 'l';
+	int b = 'b';
+	int e = 'e';
+	int r = 'r';
+	int t = 't';
+	int z = 'o';
+	int n = 'n';
+	_putchar(h);
+	_putchar(o);
+	_putchar(l);
+	_putchar(b);
+	_putchar(e);
+	_putchar(r);
+	_putchar(t);
+	_putchar(z);
+	_putchar(n);
+	_putchar('\n');
+	return (0);
+}
