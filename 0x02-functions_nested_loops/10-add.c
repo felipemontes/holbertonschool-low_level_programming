140681d8d19bc575649d7763f7a9083cd6624ca6
felipe <felipe_londono1@hotmail.com>
2019-09-24 17:56:04 +0000

    Function to add two intergers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - adds two intergers
+ * @a: Var uno
+ * Return: 1 if is lower or upper case.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
