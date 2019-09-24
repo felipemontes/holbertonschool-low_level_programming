76eb4783ad47d99ee99d4bcfa8a1ae04d374a09b
felipe <felipe_londono1@hotmail.com>
2019-09-24 20:51:37 +0000

    function that counts numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints all natural numbers
+ *
+ * Return: void
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for(; n <= 98; n++)
+		{
+		printf("%d", n);
+		if (n == 98)
+		{
+		}
+		else
+		{
+			printf(", ");
+		}
+		}
+	}
+	else if (n > 98)
+	{
+		for(; n >= 98; n--)
+		{
+		printf("%d", n);
+		if (n == 98)
+		{
+		}
+		else
+		{
+			printf(", ");
+		}
+		}
+	}
+printf("\n");
+}
