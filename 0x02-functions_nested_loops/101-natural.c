115ab4cc4b7be452baeb92848fadb26708a29804
felipe <felipe_londono1@hotmail.com>
2019-09-25 21:07:22 +0000

    program to sum natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - This program is about the sum of natural numbers
+ * Return: It returns 0
+ */
+
+int main(void)
+{
+	long int a = 0;
+	long int sum = 0;
+
+	while (a < 1024)
+	{
+		if ((a % 3 == 0) || (a % 5 == 0))
+		{
+			sum = sum + a;
+		}
+		a++;
+	}
+	printf("%lu", sum);
+	return (0);
+}
