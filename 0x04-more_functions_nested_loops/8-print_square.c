eced0dd4993c2884a3eebc0c96b2b64eabf59f26
felipe <felipe_londono1@hotmail.com>
2019-09-26 19:54:33 +0000

    Program that prints squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,22 +8,22 @@
  */
 void print_square(int size)
 {
-        int a;
+	int a;
 	int b;
 
-        if (size <= 0)
-        {
-                _putchar('\n');
-        }
-        else
-        {
-		for( a = 1; a <= size; a++)
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 1; a <= size; a++)
 		{
-			for(b = 0; b < size; b++)
+			for (b = 0; b < size; b++)
 			{
 				_putchar('#');
 			}
 			_putchar('\n');
 		}
-        }
+	}
 }
