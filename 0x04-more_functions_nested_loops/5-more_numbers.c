da2fd2a483c100230b01176daad59a20c395e1ac
felipe <felipe_londono1@hotmail.com>
2019-09-26 17:44:32 +0000

    Fixed indentation

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,18 +7,19 @@
  */
 void more_numbers(void)
 {
-        int a;
+	int a;
 	int b;
+
 	for (a = 0; a < 10; a++)
 	{
-        for (b = 0; b < 15; b++)
-        {
-		if (b >= 10)
+		for (b = 0; b < 15; b++)
 		{
-			_putchar((b / 10) + '0');
-		}
+			if (b >= 10)
+			{
+				_putchar((b / 10) + '0');
+			}
 			_putchar((b % 10) + '0');
-        }
-	_putchar('\n');
+		}
+		_putchar('\n');
 	}
 }
