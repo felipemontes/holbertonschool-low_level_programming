98fbebb1b1c6d7fca21ad786e6d951604ce166b6
felipe <felipe_londono1@hotmail.com>
2019-09-29 00:59:01 +0000

    Fixed long code

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_number - function to print any interger
+ *@n: variable
+ * Return: void
+ */
+void print_number(int n)
+{
+        unsigned int number;
+
+        number = n;
+        if (n < 0)
+        {
+                number = number * -1;
+                _putchar ('-');
+        }
+        if (n / 10)
+        {
+                print_number(number / 10);
+        }
+        _putchar(number % 10 + '0');
+}
