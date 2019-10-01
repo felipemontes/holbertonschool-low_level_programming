84e207f5d60900c52bd9fd8f5988960e8a59ccf2
felipe <felipe_londono1@hotmail.com>
2019-10-01 15:36:47 +0000

    Added iterator

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,16 @@
  */
 void puts2(char *str)
 {
+	int len = 0;
 	int i;
-	char b;
 
-	for (i = 0; str[i] != '\0' ; i += 2)
+	while (str[len] != '\0')
 	{
-		b = str[i];
-		_putchar(b);
+		len++;
+	}
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
