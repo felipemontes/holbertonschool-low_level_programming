a80ccaa9c7ab7235395cd83a2d2500258b6a65ca
felipe <felipe_londono1@hotmail.com>
2019-10-03 21:33:44 +0000

    function to convert first letter to iupper

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * *cap_string - converts firt letter to upper
+ * @s: string
+ * Return: string
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == 0)
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+			}
+		}
+		if (s[i] == ' ')
+		{
+			++i;
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+			}
+		}
+	}
+	return (s);
+}
