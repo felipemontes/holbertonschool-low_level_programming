d7d2102cc263f4e4c11971f1b775e2c48bbd45dd
felipe <felipe_londono1@hotmail.com>
2019-10-03 21:00:39 +0000

    function to convert a string to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * *string_toupper - converts a string to upper
+ * @str: string
+ * Return: string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
