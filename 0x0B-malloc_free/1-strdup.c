5bd6f33f0690cfd5ea13289cc48be1386bc27bb5
felipe <felipe_londono1@hotmail.com>
2019-10-15 18:31:38 +0000

    function to return a newly allocated stt

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strlen - swap value of two intergers
+ * @s: string
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
+/**
+ * *_strcpy - copies string pointed to srd
+ * @dest: dest array
+ * @src: src array
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+ * _strdup - write a function that returns a pointers to a newly allocated str
+ * @str: string
+ * Return: void
+ */
+char *_strdup(char *str)
+{
+	char *a;
+
+	a = malloc(_strlen(str) * sizeof(char);
+	_strcpy(a, str);
+	return (a);
+}
