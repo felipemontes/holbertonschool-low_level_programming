b9727415e3a79d3130bdc015298af24c0533c17d
felipe <felipe_londono1@hotmail.com>
2019-10-23 16:57:17 +0000

    function to print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that prints a name
+ * @name: name
+ * @f: function
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+	{
+	}
+	else
+	{
+	(*f)(name);
+	}
+}
