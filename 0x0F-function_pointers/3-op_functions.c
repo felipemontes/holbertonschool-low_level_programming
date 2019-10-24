26397f72e6902a1b5c6428c43db4d0eea33e109d
felipe <felipe_londono1@hotmail.com>
2019-10-24 01:49:36 +0000

    funtions of the calculator

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * op_add - adds two ints
+ * @a: firts int
+ * @b: second int
+ * Return: the sum of two ints
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substract two ints
+ * @a: firts int
+ * @b: second int
+ * Return: the substract of two ints
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply two ints
+ * @a: firts int
+ * @b: second int
+ * Return: the multiplicatiopn of two ints
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide two ints
+ * @a: firts int
+ * @b: second int
+ * Return: the div of two ints
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - mod of two ints
+ * @a: firts int
+ * @b: second int
+ * Return: the mod of two ints
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
