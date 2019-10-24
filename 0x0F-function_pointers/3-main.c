fa0ac2e9984a37b879230cc0a000ee0656529856
felipe <felipe_londono1@hotmail.com>
2019-10-24 01:46:47 +0000

    main function of the calculator

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - calculator
+ * @argc: amount of elements
+ * @argv: array
+ * Return: 0 always
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int res;
+	int (*op)(int, int);
+
+	if (argc > 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*' &&
+	    *argv[2] != '/' && *argv[2] != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && (atoi(argv[3]) == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	op = get_op_func(argv[2]);
+	res = op(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", res);
+
+	return (0);
+}
