80dc955282a83d42266610a21b3feb0efa6b73a5
felipe <felipe_londono1@hotmail.com>
2019-10-26 17:38:43 +0000

    [FIX] added null conditional

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", b);
 		}
-		if (i < n - 1)
+		if (i < n - 1 && separator != NULL)
 		{
 			printf("%c ", *separator);
 		}
