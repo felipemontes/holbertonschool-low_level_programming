ff9ddfc3c99a505585281e6f1c062e638f837ffd
felipe <felipe_londono1@hotmail.com>
2019-10-31 23:43:48 +0000

    [FEATURE]function executed before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * myStartupFun - function to print before main function
+ *
+ *
+ */
+void myStartupFun(void)__attribute__((constructor));
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon myback!\n");
+}
