697c3d3ebd925cb2106feae68ef01043afb43e9b
felipe <felipe_londono1@hotmail.com>
2019-10-31 23:46:53 +0000

    [FEATURE]function executed before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 /**
  * myStartupFun - function to print before main function
- *
- *
  */
-void myStartupFun(void)__attribute__((constructor));
+void myStartupFun(void) __attribute__ ((constructor));
 void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
