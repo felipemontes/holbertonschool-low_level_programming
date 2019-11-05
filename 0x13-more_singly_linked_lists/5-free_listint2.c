7e5bdfae6778d938a37a74195dfeed592061f301
felipe <felipe_londono1@hotmail.com>
2019-11-05 02:08:25 +0000

    [FEATURE]function that frees a list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a list
+ * @head: starting point of the list
+ *
+ *
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp = *head;
+	listint_t *next;
+
+	while (tmp != NULL)
+	{
+		next = tmp->next;
+		free(tmp);
+		tmp = next;
+	}
+	*head = NULL;
+}
