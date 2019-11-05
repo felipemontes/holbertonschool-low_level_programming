9a775bf7ad0a895987f4ecaf4ed2d7f565bc3af3
felipe <felipe_londono1@hotmail.com>
2019-11-05 17:33:07 +0000

    [FEATURE]: inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new = malloc(sizeof(listint_t));
 	listint_t *cur = *head;
 
-	if (head == NULL && !*head)
+	if (head == NULL)
 	{
 		return (NULL);
 	}
