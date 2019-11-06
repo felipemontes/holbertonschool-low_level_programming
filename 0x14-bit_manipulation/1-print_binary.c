a4b1476e823f85ed98c74be159b2cbe247f1d884
felipe <felipe_londono1@hotmail.com>
2019-11-06 03:55:51 +0000

    [ADD]: function to convert from bin to int

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,17 @@
 #include "holberton.h"
 #include <unistd.h>
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
  * print_binary - function to tranform to binary
  * @n: number
  *
@@ -12,15 +23,4 @@ void print_binary(unsigned long int n)
 		print_binary(n >> 1);
 	}
 	_putchar((n & 1) + '0');
-}
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
