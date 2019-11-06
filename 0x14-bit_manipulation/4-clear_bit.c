6f853a9a9cfaedd5dcb4c4030b619fa594838ac7
felipe <felipe_londono1@hotmail.com>
2019-11-06 06:14:38 +0000

    [ADD]: write a function to set the value of a bit to 0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * clear_bit - function to set the value of a bit to 0
+ * @n: number
+ * @index: index number
+ * Return: 1 if it worked or -1 if not
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (index > 64)
+	{
+		return (-1);
+	}
+	while (i <= index)
+	{
+		*n = (*n & (~1 << index));
+		i++;
+	}
+	return (1);
+
+}
