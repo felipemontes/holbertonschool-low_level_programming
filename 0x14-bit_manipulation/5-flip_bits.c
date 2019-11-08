187c67963e9f6ae70c04b4ef692f9af0d0539954
felipe <felipe_londono1@hotmail.com>
2019-11-08 04:18:03 +0000

    [ADD]: return number of bits to flip the number

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * flip_bits - number of bits to flip
+ * @n: number
+ * @m: the other number
+ * Return: the number of bits
+ *
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int res, iter;
+
+	res = n ^ m;
+	iter = 0;
+
+	while (res)
+	{
+		i += res & 1;
+		res = res >> 1;
+	}
+	return (i);
+}
