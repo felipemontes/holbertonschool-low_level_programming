50104cd1c46871c0ab37a33a180763da7375008b
felipe <felipe_londono1@hotmail.com>
2019-11-12 17:44:25 +0000

    [ADD]: function that reads a text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * read_textfile - function to read text
+ * @filename: file name
+ * @letters: letter
+ * Return: the actual number of letters
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fp;
+	char *buf = malloc(sizeof(letters));
+	int last;
+
+	if (filename == NULL || buf == NULL)
+	{
+		return (0);
+	}
+	fp = open(filename, O_RDONLY);
+	if (fp == -1)
+	{
+		return (0);
+	}
+	last = read(fp, buf, letters);
+	write(1, buf, letters);
+	close(fp);
+	free(buf);
+	return (last);
+}
