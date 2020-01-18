5644547329a2d8a4d2500b92491f0965af721bbe
Felipe <felipe_londono1@hotmail.com>
2020-01-18 18:47:43 +0000

    [FIX]: corrected malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
-	ht->array = malloc(size * sizeof(hash_node_t));
+	ht->array = malloc(size * sizeof(hash_node_t *));
 	if (ht->array == NULL)
 		return (NULL);
 	ht->size = size;
